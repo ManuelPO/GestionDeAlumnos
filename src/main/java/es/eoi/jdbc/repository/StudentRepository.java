8d91bdb25299c9645428b2cdcc5d71dd53c96729
Manuel <manuel.prietoor@gmail.com>
2022-05-31 13:17:35 +0200

    first part

--- src/main/java/es/eoi/jdbc/repository/StudentRepository.java
+++ src/main/java/es/eoi/jdbc/repository/StudentRepository.java
@@ -2,16 +2,19 @@ package es.eoi.jdbc.repository;
 
 import es.eoi.jdbc.entity.Student;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface StudentRepository {
 
+    Student findById(Integer id) throws SQLException;
 
-    public Student findById(String id);
-    public List<Student> findAll();
-    public Student create(Student student);
-    public boolean delete(String id);
-    public Student update(Student student);
+    List<Student> findAll() throws SQLException;
 
+    Integer create(Student student) throws SQLException;
+
+    boolean delete(String id) throws SQLException;
+
+    Integer update(Student student) throws SQLException;
 
 }
--- src/main/java/es/eoi/jdbc/service/StudentService.java
+++ src/main/java/es/eoi/jdbc/service/StudentService.java
@@ -4,30 +4,62 @@ import es.eoi.jdbc.entity.Student;
 import es.eoi.jdbc.repository.StudentRepository;
 import es.eoi.jdbc.repository.StudentRepositoryImpl;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class StudentService {
 
-    private final StudentRepository studentRepository;
+    private final StudentRepository repository;
 
     public StudentService() {
-
-        studentRepository = new StudentRepositoryImpl();
+        this.repository = new StudentRepositoryImpl();
     }
 
-    public StudentService(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+    public Student findById(Integer id) {
+        try {
+            return this.repository.findById(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(String.format("Error trying to find student %d", id), e.getCause());
+        }
     }
 
-    public Student findById(String id);
+    public List<Student> findAll() {
+        try {
+            return this.repository.findAll();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error trying get all students", e.getCause());
+        }
+    }
 
-    public List<Student> findAll();
+    public Student create(Student student) {
+        if (student.getId() != null) {
+            throw new RuntimeException(String.format("Student ID has to be null, but it is %d", student.getId()));
+        }
+        try {
+            final Integer id = this.repository.create(student);
+            return this.findById(id);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error trying create student", e.getCause());
+        }
+    }
 
-    public Student create(Student student);
+    public boolean delete(String id) {
+        try {
+            return this.repository.delete(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(String.format("Error trying to delete student %d", id), e.getCause());
+        }
+    }
 
-    public boolean delete(String id);
-
-    public Student update(Student student);
-
-
+    public Student update(Student student) {
+        if (student.getId() == null) {
+            throw new RuntimeException("Student ID has to be set but it is null");
+        }
+        try {
+            final Integer id = this.repository.update(student);
+            return this.findById(id);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error trying save student", e.getCause());
+        }
+    }
 }
